0b085d6e76cbb662abf9d678b564ae3a50d1d577
Oliver Borys <oliver.borys@gmail.com>
2025-02-17 17:20:34 +0100

    Small changes to server.js according to lecture

--- backend/server.js
+++ backend/server.js
@@ -167,5 +167,5 @@ app.post("/api/update-hero-images", (req, res) => {
 
   
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port: ${PORT}`);
 });
