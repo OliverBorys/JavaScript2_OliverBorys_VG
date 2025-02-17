faf949d595f292c3f10a7d25978befe179abcbbd
Oliver Borys <oliver.borys@gmail.com>
2025-02-17 11:15:42 +0100

    Added AdminPage and styling to it, made deletebutton and add product functional

---
+++ src/components/Admin/AdminPage/AdminUserSection.jsx
@@ -0,0 +1,10 @@
+const AdminUserSection = () => {
+    return (
+      <div>
+        <p className="text-gray-700">Manage Admin Users (Coming Soon...)</p>
+      </div>
+    );
+  };
+  
+  export default AdminUserSection;
+  
--- src/pages/AdminPage/AdminPage.jsx
+++ src/pages/AdminPage/AdminPage.jsx
@@ -1,7 +1,34 @@
+import { useEffect } from "react";
+import Accordion from "../../components/Admin/AdminPage/Accordion";
+import ProductTable from "../../components/Admin/AdminPage/ProductTable";
+import HeroImageForm from "../../components/Admin/AdminPage/HeroImageForm";
+import AdminUserSection from "../../components/Admin/AdminPage/AdminUserSection.jsx";
 
 const AdminPage = () => {
-    return <h1 className="text-center text-2xl mt-20 font-bold">Admin Page</h1>;
-  };
-  
-  export default AdminPage;
-  
+  useEffect(() => {
+    document.title = "Admin Panel";
+  }, []);
+
+  return (
+    <main className="mt-28 p-4 md:p-6 md:mx-6 lg:mx-16">
+      <header className="text-center my-8">
+        <h1 className="text-2xl font-medium text-gray-800">Shop</h1>
+      </header>
+      <section className="w-full">
+        <Accordion title="Products Administration">
+          <ProductTable />
+        </Accordion>
+
+        <Accordion title="Hero Pictures">
+          <HeroImageForm />
+        </Accordion>
+
+        <Accordion title="Admin User">
+          <AdminUserSection />
+        </Accordion>
+      </section>
+    </main>
+  );
+};
+
+export default AdminPage;
