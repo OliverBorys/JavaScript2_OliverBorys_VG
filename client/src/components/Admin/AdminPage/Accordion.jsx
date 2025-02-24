01ff3e052ef56a062e572d5a1f3de9f2fdac2149
Oliver Borys <oliver.borys@gmail.com>
2025-02-24 17:23:29 +0100

    Changed color of AdminAccordion to match footer and on the ContactPage

--- client/src/components/Admin/AdminPage/Accordion.jsx
+++ client/src/components/Admin/AdminPage/Accordion.jsx
@@ -7,18 +7,18 @@ const Accordion = ({ title, children }) => {
   return (
     <div className="mb-4">
       <button
-        className="accordion-button flex justify-between items-center w-full bg-white text-black text-md font-medium px-6 py-4 border border-blue-200 rounded-md hover:bg-blue-200 transition"
+        className="accordion-button flex justify-between items-center w-full bg-white text-[#3b0001] text-md font-medium px-6 py-4 border border-[#f7e0c8] rounded-md hover:bg-[#f7e0c8] transition"
         onClick={() => setIsOpen(!isOpen)}
       >
         {title}
         <span
-          className={`accordion-icon text-blue-500 transition-transform duration-700 ${isOpen ? "rotate-180" : ""}`}
+          className={`accordion-icon text-[#3b0001] transition-transform duration-700 ${isOpen ? "rotate-180" : ""}`}
         >
           ▼
         </span>
       </button>
       <div
-        className={`panel bg-gray-50 px-6 py-4 border border-t-0 rounded-md border-blue-200 ${isOpen ? "block" : "hidden"}`}
+        className={`panel bg-gray-50 px-6 py-4 border border-t-0 rounded-md border-[#f7e0c8] ${isOpen ? "block" : "hidden"}`}
       >
         {children}
       </div>
