9524ce2413f08e0707de66d5b1fee628c0cee36e
Oliver Borys <oliver.borys@gmail.com>
2025-03-06 13:03:34 +0100

    changed back to contact table as before because the styling was not as desiered

--- client/src/components/Contact/ContactAccordion.jsx
+++ client/src/components/Contact/ContactAccordion.jsx
@@ -5,14 +5,23 @@ const ContactAccordion = ({ title, children }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <section className="w-full border-b border-gray-900 last:border-b-0">
-      <button className="flex justify-between items-center w-full text-gray-900 text-lg font-normal py-5 focus:outline-none" onClick={() => setIsOpen(!isOpen)}>
+    <div className="w-full border-b border-gray-900 last:border-b-0">
+      <button
+        className="flex justify-between items-center w-full text-gray-900 text-lg font-normal py-5 focus:outline-none"
+        onClick={() => setIsOpen(!isOpen)}
+      >
         <span className="text-xl font-medium">{title}</span>
-        <span className="text-gray-900 text-2xl">{isOpen ? "−" : "+"}</span>
+        <span className="text-gray-900 text-2xl">
+          {isOpen ? "−" : "+"}
+        </span>
       </button>
 
-      {isOpen && <div className="py-4 space-y-4 text-gray-900 text-md">{children}</div>}
-    </section>
+      {isOpen && (
+        <div className="py-4 space-y-4 text-gray-900 text-md">
+          {children}
+        </div>
+      )}
+    </div>
   );
 };
 
--- client/src/components/Contact/PurchaseInformationTable.jsx
+++ client/src/components/Contact/PurchaseInformationTable.jsx
@@ -1,32 +1,27 @@
-import InfoTable from "./InfoTable";
-
 const PurchaseInformationTable = () => {
   return (
-    <>
-      <InfoTable
-        title="Purchase Methods"
-        content={
-          <>
-            <p>We accept:</p>
-            <ul className="list-disc pl-5 text-gray-900">
-              <li>Visa, Mastercard</li>
-              <li>American Express</li>
-              <li>Swish</li>
-              <li>Klarna</li>
-            </ul>
-          </>
-        }
-      />
-
-      <InfoTable
-        title="Duty & Tax"
-        content={
+    <section className="border-t -mt-4 py-4">
+      <div className="col-span-2">
+        <h2 className="uppercase font-medium">Purchase methods</h2>
+        <p>We accept:</p>
+        <ul className="list-disc pl-5 text-gray-900">
+          <li>Visa, Mastercard</li>
+          <li>American Express</li>
+          <li>Swish</li>
+          <li>Klarna</li>
+        </ul>
+      </div>
+      <div className="col-span-3 mt-2 bg-gray-400 h-[1px] mr-0"></div>
+      <div className="col-span-2 mt-4">
+        <h2 className="uppercase font-medium">Duty & tax</h2>
           <p>
-            No additional import taxes or duties apply unless shipped to Russia or Canada.
+            You will not be charged any additional import taxes or duties on
+            receipt of your item, unless your order is being shipped to Russia
+            or Canada. Orders shipped to Russia and Canada will be subject to
+            customs duties on import.
           </p>
-        }
-      />
-    </>
+      </div>
+    </section>
   );
 };
 
