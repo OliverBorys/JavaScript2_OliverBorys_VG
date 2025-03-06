e74e3222205c808ce4cfd8d76a6392ee21e8da16
Oliver Borys <oliver.borys@gmail.com>
2025-03-06 18:02:47 +0100

    Deleted header from product details page so it doesn't render 2 times

--- client/src/pages/ShopPage/ProductDetailsPage.jsx
+++ client/src/pages/ShopPage/ProductDetailsPage.jsx
@@ -1,32 +1,25 @@
-import { useState } from "react";
 import { useParams } from "react-router-dom";
 import useProduct from "../../hooks/useProduct";
 import ProductImage from "../../components/Shop/ProductDetails/ProductImage";
 import ProductInfo from "../../components/Shop/ProductDetails/ProductInfo";
 import Swiper from "../../components/Shop/ProductDetails/Swiper";
-import Header from "../../components/Header/Header";
 import MissingProduct from "../../components/Shop/ProductDetails/MissingProduct";
 
 const ProductDetailsPage = () => {
   const { id } = useParams();
   const { product, loading, error } = useProduct(id);
-  const [isCartSidebarOpen, setCartSidebarOpen] = useState(false);
 
-  const toggleCartSidebar = () => setCartSidebarOpen((prev) => !prev);
 
   if (loading) return <p className="text-center text-gray-500">Loading...</p>;
   if (error) return <MissingProduct />;
 
   return (
     <>
-      <Header
-        isCartSidebarOpen={isCartSidebarOpen}
-        toggleCartSidebar={toggleCartSidebar}
-      />
+     
       <main className="mt-14 sm:mt-20 lg:mt-24">
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 items-start">
           <ProductImage product={product} />
-          <ProductInfo product={product} onCartUpdate={toggleCartSidebar} />
+          <ProductInfo product={product} />
         </div>
         <Swiper product={product} />
       </main>
